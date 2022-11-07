48b260494794304883fb95de8ccf1db76ff31ca0
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2022-11-07 01:20:03 -0500

    add remove_duplicate_files function

---
+++ remove_duplicate_files.py
@@ -0,0 +1,29 @@
+from dir_walker import dir_walker
+import hashlib
+import os
+
+
+def remove_duplicate_files(dir_path, auto_delete=False):
+    seen = set()
+    for file in dir_walker(dir_path):
+        with open(file, "rb") as f:
+            file_hash = hashlib.md5(f.read()).hexdigest()
+        if file_hash in seen:
+            if auto_delete:
+                os.remove(file)
+            else:
+                print(f"Duplicate file: {file}")
+                input("Remove? [yes/no] ")
+                if input() == "yes":
+                    os.remove(file)
+        else:
+            seen.add(file_hash)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python remove_duplicate_files.py <dir>")
+        sys.exit(1)
+    remove_duplicate_files(sys.argv[1])
