020851042cb9ffc0e66a6165b478c441081966ec
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2022-11-12 23:52:38 -0500

    add rename files command

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import typer
 from remove_duplicate_files import remove_duplicate_files
+from rename_files import rename_files
 
 
 app = typer.Typer()
 
 
 @app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
+def duplicates(dir_path: str, autodelete: bool = False):
+    remove_duplicate_files(dir_path, autodelete)
 
 
 @app.command()
-def duplicates(dir_path: str, autodelete: bool = False):
-    remove_duplicate_files(dir_path, autodelete)
-    typer.echo(f"Duplicate files removed from {dir_path}")
+def rename(dir_path: str):
+    rename_files(dir_path)
+
 
 if __name__ == "__main__":
     app()
