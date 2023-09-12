6710df1a41a9bddd8e283703db6983fc289c0229
Alejandro Sanchez Yali <asanchezyali@gmail.com>
2023-09-11 22:47:26 -0500

    refactor rename files method

--- src/rename_files.py
+++ src/rename_files.py
@@ -3,9 +3,112 @@ import signal
 import subprocess
 from src.dir_walker import dir_walker
 from utils.bcolors import print_info, print_header, print_success
-from src.rename_article_or_book import rename_article_or_book
+from src.rename_article_or_book import (
+    rename_file,
+    rename_article,
+    rename_book,
+    rename_thesis,
+)
 from src.rename_invoice_or_report import rename_invoice_or_report_contract
 from src.filename_formats import invoice_validator, article_validator
+from src.constants import (
+    ARTICLE,
+    BOOK,
+    THESIS,
+    COMMERCIAL_DOCUMENT,
+    LEGAL_DOCUMENT,
+    SCRIPT,
+    NON_DISCLOSURE_AGREEMENT,
+)
+
+document_types = [
+    ARTICLE,
+    BOOK,
+    THESIS,
+    COMMERCIAL_DOCUMENT,
+    LEGAL_DOCUMENT,
+    SCRIPT,
+    NON_DISCLOSURE_AGREEMENT,
+]
+
+actions = {
+    ARTICLE: rename_article,
+    BOOK: rename_book,
+    THESIS: rename_thesis,
+}
+
+
+def contains_document_type(filename):
+    for doc_type in document_types:
+        if "." + doc_type + "." in filename:
+            return True
+    return False
+
+
+def get_filename(file):
+    return os.path.basename(file)
+
+
+def set_filename(option, file, filename):
+    actions[document_types[option - 1]](file, get_extension(filename))
+
+
+def get_extension(filename):
+    return os.path.splitext(filename)[1]
+
+
+def open_file(file):
+    process = subprocess.Popen(
+        ["xdg-open", file], stdout=subprocess.PIPE, preexec_fn=os.setsid
+    )
+    return process
+
+
+def close_file(process):
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+
+
+def show_options(filename):
+    print_header(f"Select an document type for {filename}:")
+    for i in range(len(document_types)):
+        print_info(f"{i+1}. {document_types[i]}")
+    print_info(f"{len(document_types)+1}. Delete file")
+    print_info(f"{len(document_types)+2}. Exit")
+
+
+def delete_file(file):
+    sure = str(input("Are you sure? [yes/no] ")).lower()
+    while sure not in ["yes", "y", "no", "n"]:
+        sure = str(input("Are you sure? [yes/no] ")).lower()
+    if sure == "yes" or sure == "y":
+        os.remove(file)
+
+
+def rename_all_files(dir_path):
+    print_header("Starting rename files...")
+    for file in dir_walker(dir_path):
+        filename = get_filename(file)
+        if contains_document_type(filename):
+            continue
+        process = open_file(file)
+        show_options(filename)
+        option = int(input("Option: "))
+        while option not in range(1, 10):
+            option = int(input("Option: "))
+        if option == len(document_types) + 1:
+            close_file(process)
+            delete_file(file)
+            continue
+        elif option == len(document_types) + 2:
+            close_file(process)
+            print_info("Exiting...")
+            break
+        close_file(process)
+        renamed = set_filename(option, file, filename)
+        while not renamed:
+            renamed = set_filename(option, file, filename)
+        print_success("File renamed successfully!")
+    print_success("Finished renaming files!")
 
 
 def get_intent(file, extension):
@@ -25,9 +128,9 @@ def get_intent(file, extension):
                 )
             ).lower()
         if doc_type == "article" or doc_type == "book":
-            renamed = rename_article_or_book(doc_type, file, extension)
+            renamed = rename_file(doc_type, file, extension)
             while not renamed:
-                renamed = rename_article_or_book(doc_type, file, extension)
+                renamed = rename_file(doc_type, file, extension)
         elif doc_type == "invoice" or doc_type == "report" or doc_type == "contract":
             renamed = rename_invoice_or_report_contract(doc_type, file, extension)
             while not renamed:
